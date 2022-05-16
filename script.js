9ea5a41260664a59639f9bf37898c8b019da483f
Tom Espen Pedersen <tom.espen.pedersen@gmail.com>
2022-05-16 08:52:49 +0200

    Fixed a couple bugs

--- script.js
+++ script.js
@@ -2,20 +2,39 @@ let s1 = new Audio('sounds/s1.wav');
 let s2 = new Audio('sounds/s2.wav');
 let nIntervId;
 
-queue = [];
-beatsIn1Min = 60000;
+let queue = [];
+let beatsIn1Min = 60000;
+let beatcount;
 
-let gtempoelemval = document.getElementById('globaltempo').value;
-
-let tempointerval = beatsIn1Min / gtempoelemval;
-console.log(beatsIn1Min + ' / ' + gtempoelemval + ' = ' + tempointerval);
 
 function start() {
+    
     console.log('starting...');
+    let beatcount = 0;
     if (!nIntervId) {
-        nIntervId = setInterval(plays1, tempointerval);
+        
+        console.log(beatcount);        
+        let gtempoelemval = document.getElementById('globaltempo').value;
+        let tempointerval = beatsIn1Min / gtempoelemval;
+        console.log(beatsIn1Min + ' / ' + gtempoelemval + ' = ' + tempointerval);
+
+        nIntervId = setInterval(play, tempointerval);
       }
 };
+
+function play() {
+    beatcount++;
+    s1.pause();
+    s1.currentTime = 0;
+    s2.pause();
+    s2.currentTime = 0;
+
+    if ((beatcount-1) % 4) {
+        plays2();
+    } else {
+        plays1();
+    }
+}
 
 function stopinterval() {
     console.log('stopping...');
@@ -26,4 +45,8 @@ function stopinterval() {
 function plays1() {
     console.log('ding');
     s1.play();
+}
+function plays2() {
+    console.log('ding');
+    s2.play();
 }
